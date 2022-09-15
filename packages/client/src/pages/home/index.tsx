99f1349ea7611cda0680587be6d836c132b9fc57
uloveits <719310130@qq.com>
2022-09-15 19:34:25 +0800

    feat✨:  梅尔图谱

--- packages/client/src/pages/home/index.tsx
+++ packages/client/src/pages/home/index.tsx
@@ -5,8 +5,9 @@
  */
 
 import { Button } from 'antd';
-import Plotly from 'plotly.js';
+import Plotly from 'plotly.js-dist-min';
 import React from 'react';
+import moment from 'moment';
 import Plot, { Figure } from 'react-plotly.js';
 import { AutoSize } from 'ronds-react-ui';
 import mel_data from './mel_data.json';
@@ -20,13 +21,21 @@ const Home = () => {
   };
 
   const onPlotClick = (e: any) => {
-    console.log(e);
-    console.log(e, e.points[0].x, (mel_data as any).extra[e.points[0].x]);
+    console.log(e, mel_data);
+
+    const pointIndex = e.points[0].pointIndex;
+    const x = e.points[0].data.x[pointIndex[1]];
+    const y = e.points[0].data.y[pointIndex[0]];
+    const y2 = (mel_data as any).y[pointIndex[0]];
+
+    console.log('x', x, moment(e.points[0].x).valueOf(), e.points[0].x);
+    console.log('y', y, e.points[0].y);
+    console.log('log', Math.log(y2), Math.log(y2) * 0.3);
   };
 
   const onZoom = async () => {
     if (!graphDivRef.current) return;
-
+    debugger;
     Plotly.relayout(graphDivRef.current, {
       'yaxis.autorange': true,
       'xaxis.autorange': true,
@@ -42,13 +51,13 @@ const Home = () => {
               <Button onClick={onZoom}>reset zoom</Button>
 
               <Plot
+                divId="test"
                 data={[
                   {
                     x: (mel_data as any).x,
                     y: (mel_data as any).y,
                     z: (mel_data as any).z,
                     type: 'heatmap',
-                    // colorscale: [[-1, '#000']],
                   },
                 ]}
                 layout={{
@@ -62,22 +71,22 @@ const Home = () => {
                     type: 'date',
                     tickformat: '%m-%d %H:%M',
                   },
-                  // annotations: [
-                  //   {
-                  //     arrowwidth: 1,
-                  //     arrowhead: 0,
-                  //     showarrow: true,
-                  //     x: '2021-09-19 12:00:00',
-                  //     y: 3,
-                  //     text: '2,3',
-                  //     font: { color: 'yellow' },
-                  //     // ay: 0,
-                  //     // ax: 0,
-                  //     // valign: 'middle',
-                  //     // xanchor: 'left',
-                  //     arrowcolor: 'yellow',
-                  //   },
-                  // ],
+                  annotations: [
+                    {
+                      arrowwidth: 1,
+                      arrowhead: 0,
+                      showarrow: true,
+                      x: '2021-09-29 12:00:00',
+                      y: Math.log(4096) * 0.30102999566,
+                      text: '2,3',
+                      font: { color: 'yellow' },
+                      ay: 17,
+                      ax: -14,
+                      valign: 'middle',
+                      xanchor: 'left',
+                      arrowcolor: 'yellow',
+                    },
+                  ],
                 }}
                 config={{
                   displayModeBar: false,
